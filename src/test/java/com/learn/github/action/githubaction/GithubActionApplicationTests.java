c493b340d2610c8f437d33a8daf924dec506968b
Gomding <backchida@gmail.com>
2022-02-16 13:57:38 +0900

    feat : 실패하는 테스트 추가

--- src/test/java/com/learn/github/action/githubaction/GithubActionApplicationTests.java
+++ src/test/java/com/learn/github/action/githubaction/GithubActionApplicationTests.java
@@ -3,11 +3,18 @@ package com.learn.github.action.githubaction;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 @SpringBootTest
 class GithubActionApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void failTest() {
+		assertThat("1234").isEqualTo("4321");
 	}
 
 }
